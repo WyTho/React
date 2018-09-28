dcbbadd17036ea52e002ce241127bcb79b94b8a9
eclipticrick <eclipticrick@gmail.com>
2018-09-28 17:11:15 +0200

    added tslint.json

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 
 interface IProps {
     name: string
 }
 
 export class App extends React.Component<IProps, {}> {
-    render() {
+    public render() {
         return <h2>Hello, {this.props.name}!</h2>
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from "./components/App";
-import "./styles/style.scss";
+import { App } from './components/App';
+import './styles/style.scss';
 
 const ROOT = document.getElementById('app');
 
