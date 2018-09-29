00ac852e99707d9301be11ff3c61fc050bdd7644
eclipticrick <eclipticrick@gmail.com>
2018-09-29 16:00:25 +0200

    added a Test for Material-UI features (Grid, Tooltip & Styling)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { Grid, Paper, Tooltip, Typography } from '@material-ui/core';
 import * as React from 'react';
 
 interface IProps {
@@ -6,6 +7,25 @@ interface IProps {
 
 export class App extends React.Component<IProps, {}> {
     public render() {
-        return <h2>Hello, {this.props.name}!</h2>
+
+        const testStyle = { marginTop: 20, padding: 20, cursor: 'pointer' };
+        const testHandler = () => alert('awesome');
+
+        return (
+            <Grid container spacing={16} justify='center'>
+                <Grid item xs={6}>
+                    <Tooltip title='This works!'>
+                        <Paper elevation={1} style={testStyle} onClick={testHandler}>
+                            <Typography variant='headline' component='h3'>
+                                Hello, {this.props.name}!
+                            </Typography>
+                            <Typography component='p'>
+                                This is a sheet of paper.
+                            </Typography>
+                        </Paper>
+                    </Tooltip>
+                </Grid>
+            </Grid>
+        )
     }
 }
