71ab58274ee1f1813b0f5b2040e5eabb08927654
eclipticrick <eclipticrick@gmail.com>
2018-10-05 12:53:10 +0200

    Added generic 'show dialog' functionality

--- src/UI/Modal/Modal.tsx
+++ src/UI/Modal/Modal.tsx
@@ -3,12 +3,13 @@ import {Dialog, DialogTitle } from '@material-ui/core';
 import * as React from 'react';
 interface IModal {
     children: any,
+    onClosed: any,
     opened: boolean,
     title: string
 }
 
 const modal = (props: IModal) => (
-    <Dialog onClose={() => {alert('onClose')}} open={props.opened}>
+    <Dialog onClose={props.onClosed} open={props.opened}>
         <DialogTitle>{props.title}</DialogTitle>
         {props.children}
     </Dialog>
--- src/containers/Overview/Overview.tsx
+++ src/containers/Overview/Overview.tsx
@@ -1,16 +1,57 @@
+import { List, ListItem, ListItemAvatar, ListItemText, Avatar } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import * as React from 'react';
+import Modal from '../../UI/Modal/Modal';
 import './Overview.scss';
 
-export class Overview extends React.Component<{}, {}> {
+interface IState {
+    modal: {
+        opened: boolean
+    }
+}
+
+export class Overview extends React.Component<{}, IState> {
+    public state = {
+        modal: {
+            opened: false
+        }
+    };
 
     public render() {
 
         return (
             <div className='Overview'>
-
+                <button onClick={this.openModalHandler}>Open Modal</button>
+                {this.modalJSX()}
             </div>
         )
     }
 
+    private openModalHandler = () => {
+        this.setState({ modal: { opened: true } } )
+    };
+
+    private modalJSX = () => (
+        <Modal title={'Hello planet!'}
+               opened={this.state.modal.opened}
+               onClosed={() => this.setState({ modal: { opened: false } } )}>
+            <div>
+                <List>
+
+                    <ListItem button>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <AddIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary='add nothing' />
+                    </ListItem>
+
+                </List>
+            </div>
+        </Modal>
+    );
+
 }
+
 export default Overview;
