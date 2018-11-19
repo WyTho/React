b708c4a911393f0407d8c299e8b8e4869afef183
eclipticrick <eclipticrick@gmail.com>
2018-11-19 12:47:46 +0100

    styled the item (device) information pop-up

--- src/components/Popup/PopupContent/ItemPopup/ItemPopup.tsx
+++ src/components/Popup/PopupContent/ItemPopup/ItemPopup.tsx
@@ -1,58 +1,92 @@
-import {Typography} from '@material-ui/core';
+import {Grid, Icon, Typography} from '@material-ui/core';
 import * as React from 'react';
 import {IPassedPopupContentProps, IReduxPopupContentProps} from '../PopupContent';
 import {beautifyDate} from '../../../../utils/date/date';
 import {IApiItem} from '../../../../utils/data/dataTypes';
+import {getIconNameForGroups} from '../../../../utils/popup/popup';
+import Loading from '../../../../components/Loading/Loading'
+
+const headerJSX = (title: string) => (
+    <Typography variant='overline' color='primary'>
+        {title}
+    </Typography>
+);
+
+const infoJSX = (title: string, values: string | string[], shouldHide?: boolean) => {
+    if (typeof values === 'string') values = [values];
+    values = values.filter(value => value);
+
+    if (shouldHide || !values.length) {
+        return null;
+    }
+
+    return (
+        <div className='infoSection'>
+            {!title ? null : <Typography variant='caption' color='secondary'>{title}</Typography>}
+            {values.map(value => <Typography key={value} variant='caption'>{value}</Typography>)}
+        </div>
+    )
+};
 
 const itemPopup = (props: IPassedPopupContentProps & IReduxPopupContentProps) => {
-    const { popup, theme } = props;
+    const { popup } = props;
     const item: IApiItem = popup.data as IApiItem;
-    return (
-        <div className='popupContent item'>
 
-            <Typography variant='overline' color='primary'>
-                information
-            </Typography>
-            <Typography variant='overline'>
-                id: {item.id ? item.id : null}
-            </Typography>
-            <Typography variant='overline'>
-                name: {item.name ? item.name : null}
-            </Typography>
-            <Typography variant='overline'>
-                address: {item.address ? item.address : null}
-            </Typography>
-            <Typography variant='overline'>
-                comment: {item.comment ? item.comment : null}
-            </Typography>
+    let content = <div className='absoluteFlexContainer'><Loading /></div>;
+    if (item) {
+        content = (
+            <div className='popupContent item'>
+                <Grid container
+                      direction='row'
+                      justify='flex-start'
+                      alignItems='stretch'>
+                    <Grid item>
+                        <Icon className='itemIcon' color='primary'>{getIconNameForGroups(item.groups)}</Icon>
+                    </Grid>
+                    <Grid item>
+                        {headerJSX('Informatie')}
+                        {infoJSX('apparaatnaam',
+                            item.name
+                        )}
+                        {infoJSX('commentaar',
+                            item.comment
+                        )}
+                        {infoJSX(
+                            `Laatste waarneming (${item.last_use.last_use.datatype})`,
+                            [
+                                item.last_use.last_use.data,
+                                beautifyDate(new Date(item.last_use.last_used * 1000), '(op {DATE} om {TIME})')
+                            ],
+                            !item.last_use.last_use.data
+                        )}
+                    </Grid>
+                </Grid>
+                <div className='verticalSpacing' />
+                <Grid container
+                      direction='row'
+                      justify='flex-start'
+                      alignItems='stretch'>
 
-            <Typography variant='overline' color='primary'>
-                Laatste verbruik ({item.last_use.last_use.datatype ? item.last_use.last_use.datatype : null})
-            </Typography>
-            <Typography variant='overline'>
-                last_use: {item.last_use.last_use.data ? item.last_use.last_use.data : null}
-                {item.last_use.last_used ? `(${beautifyDate(new Date(item.last_use.last_used * 1000), '{DATE}')})` : null}
-            </Typography>
+                    <Grid item md={6} sm={12} xs={12}>
+                        {!item.usages.length ? null : headerJSX('Verbruik')}
+                        {infoJSX(null,
+                            item.usages.map(usage => `${usage.usage} ${usage.usage_type}`)
+                        )}
+                    </Grid>
 
-            <Typography variant='overline' color='primary'>
-                usages
-            </Typography>
-            {item.usages.map(usage => (
-                <Typography key={usage.usage_type} variant='overline'>
-                    {usage.usage_type ? usage.usage_type : null}: {usage.usage ? usage.usage : null}
-                </Typography>
-            ))}
+                    <Grid item md={6} sm={12} xs={12}>
+                        {!item.groups.length ? null : headerJSX('Groep(en) waar dit apparaat in zit')}
+                        {infoJSX(null,
+                            item.groups.map(group => `- ${group.name}`)
+                        )}
+                    </Grid>
 
-            <Typography variant='overline' color='primary'>
-                groups
-            </Typography>
-            {item.groups.map(group => (
-                <Typography key={group.name} variant='overline'>
-                    {group.name ? group.name : null}
-                </Typography>
-            ))}
-        </div>
-    );
+                </Grid>
+
+            </div>
+        );
+    }
+    return content
 };
 
 export default itemPopup;
