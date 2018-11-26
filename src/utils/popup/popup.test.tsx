3f6d8392476bf6d1110eb00a749ec569c0c26df4
eclipticrick <eclipticrick@gmail.com>
2018-11-26 12:48:52 +0100

    - added popupUtils tests

---
+++ src/utils/popup/popup.test.tsx
@@ -0,0 +1,31 @@
+
+import * as popupUtils from './popup';
+
+describe('items.tsx (utils)', () => {
+
+    describe('getIconNameForGroups()', () => {
+        const func = popupUtils.getIconNameForGroups;
+
+        it('should get the icon for Lights if it has a light group (light icon has priority over any other groups)', () => {
+            expect(func([
+                { id: 0, name: 'Verlichting' }
+            ])).toBe('wb_incandescent');
+
+            expect(func([
+                { id: 0, name: 'Verlichting' },
+                { id: 1, name: 'Another_Group' },
+            ])).toBe('wb_incandescent');
+
+            expect(func([
+                { id: 0, name: 'Another_Group' },
+                { id: 1, name: 'Verlichting' },
+            ])).toBe('wb_incandescent');
+
+            expect(func([
+                { id: 0, name: 'Another_Group' }
+            ])).not.toBe('wb_incandescent');
+        });
+
+    });
+
+});
